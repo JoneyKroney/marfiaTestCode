06227f6f6f23db75731e2f94d8c1b03e676f47f5
JoneyKroney <johnlowriefirst@gmail.com>
2023-06-22 13:39:02 -0500

    added good test for normal undefined null mutiple inputs etc

--- addIsAwesome.js
+++ addIsAwesome.js
@@ -1,6 +1,9 @@
 const addIsAwesome = function (baseString, cb) {
   // your code here.
-  return baseString + " " + "is Awesome";
+  if (!(typeof baseString === 'string' || baseString instanceof String)){
+    throw new TypeError('Non String input');
+  }
+  return baseString + " is Awesome";
 };
 
 export { addIsAwesome };
--- addIsAwesome.test.js
+++ addIsAwesome.test.js
@@ -5,4 +5,46 @@ describe('addIsAwesome', () => {
   test('p input for add is awasome', () => {
     expect(addIsAwesome('p')).toEqual('p is Awesome');
   });
+
+  test('dogs input for add is awasome', () => {
+    expect(addIsAwesome('dogs')).toEqual('dogs is Awesome');
+
+  });
+  test('undefined input for add is awasome', () => {
+    expect((undefined) => addIsAwesome()).toThrow(TypeError);
+
+  });
+  test('Null input for add is awasome', () => {
+    expect((Null) => addIsAwesome()).toThrow(TypeError);
+
+  });
+  test('Multi string input for add is awasome', () => {
+    expect(addIsAwesome('p', 'p', 'p', 'p')).toEqual('p is Awesome');
+
+  });
+  test('number input for add is awasome', () => {
+    expect( addIsAwesome(1)).toThrow(TypeError);
+
+  });
+
 });
+//string inputs
+
+//dogs
+//p
+//abcdefghijklmnopqrstuvwxyz
+//pppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppp
+//1
+//123456789
+//$%/\\{}@#!*&%^$
+
+//non string inputs
+
+//Null
+//undefined
+//false true
+//number
+//object
+//fucntion
+
+// mutiple inputs into function

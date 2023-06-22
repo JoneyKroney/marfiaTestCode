66b9f51e611da8dc9578fab8190a5b91f61c2e61
JoneyKroney <johnlowriefirst@gmail.com>
2023-06-22 10:37:34 -0500

    change orgin and added test for add is addIsAwesome

--- addIsAwesome.test.js
+++ addIsAwesome.test.js
@@ -2,4 +2,7 @@ const { addIsAwesome } = require('./addIsAwesome');
 
 describe('addIsAwesome', () => {
   // your code here.
+  test('p input for add is awasome', () => {
+    expect(addIsAwesome('p')).toEqual('p is Awesome');
+  });
 });
